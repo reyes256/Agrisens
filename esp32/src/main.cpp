673eb0a88bb13aaf5f00098e4ee971e11e99bada
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-10-04 15:19:59 -0700

    ESP32 connection to broker
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -3,65 +3,122 @@
 #include <PubSubClient.h>
 
 // WiFi
-const char *ssid = "HackatecNM2"; // Enter your Wi-Fi name
-const char *password = "itesca2024";  // Enter Wi-Fi password
+const char *ssid = "HackatecNM2";
+const char *password = "itesca2024";
 
 // MQTT Broker
-const char *mqtt_broker = "10.7.84.226";
-const char *topic = "led/status";
-// const char *mqtt_username = "emqx";
-// const char *mqtt_password = "public";
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "main/topic";
 const int mqtt_port = 1883;
+const String client_id = "esp32-client-12345";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void callback(char *topic, byte *payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
   Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-      Serial.print((char) payload[i]);
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
   }
   Serial.println();
   Serial.println("-----------------------");
 }
 
-void setup() {
+void setup()
+{
   // Set software serial baud to 115200;
   Serial.begin(921600);
 
   // Connecting to a WiFi network
-  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Conectando a Internet..");
   }
-  Serial.println("Connected to the Wi-Fi network");
+  Serial.println("Conexión a Internet Establecida");
 
-  //connecting to a mqtt broker
+  // connecting to a mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
 
-  while (!client.connected()) {
-      String client_id = "esp32-client-12345";
-      // client_id += String(WiFi.macAddress());
-      Serial.printf("\nConnecting to broker...");
-      if (client.connect(client_id.c_str())) {
-        Serial.println("Connection Established");
-      } else {
-        Serial.print("failed with state ");
-        Serial.print(client.state());
-        delay(2000);
-      }
+  while (!client.connected())
+  {
+    
+    // client_id += String(WiFi.macAddress());
+    Serial.printf("\nConectando al broker... ");
+    if (client.connect(client_id.c_str()))
+    {
+      Serial.println("Conexión al Broker Establecida");
+    }
+    else
+    {
+      Serial.print("Falló con el estado: ");
+      Serial.print(client.state());
+      delay(2000);
+    }
   }
   // Publish and subscribe
   client.publish(topic, "Hi, I'm ESP32 ^^");
-  client.subscribe(topic);  
+  client.subscribe(topic);
 }
 
+void sendTemperature()
+{
+  char str[16];
+  sprintf(str, "%u", random(100));
+  client.publish("esp/temp", str);
+}
 
-void loop() {
+void sendHumidity()
+{
+  char str[16];
+  sprintf(str, "%u", random(100));
+  client.publish("esp/hum", str);
+}
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.println("\nReintentando conexión con el Broker...");
+
+    if (client.connect(client_id.c_str()))
+    {
+      Serial.println("Conexión al Broker establecida.");
+
+      if (client.subscribe(topic))
+      {
+        Serial.println("Suscripción exitosa\n");
+      }
+      else
+      {
+        Serial.println("falló Suscripción");
+      }
+    }
+    else
+    {
+      Serial.print("Falló con el estado: ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void loop()
+{
+  if (!client.connected()){
+    reconnect();
+  }
+
   client.loop();
+
+  sendTemperature();
+  sendHumidity();
+
+  delay(2000);
 }
